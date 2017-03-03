257fbc28af287d138a16986163255ab61c5b54f7
Jorryt Dijkstra <abort@Jorryts-Mac-Pro.local>
2017-03-03 19:25:32 +0100

    Fixed placing parentheses

--- algorithmic-toolbox/src/main/scala/PlacingParentheses.scala
+++ algorithmic-toolbox/src/main/scala/PlacingParentheses.scala
@@ -9,7 +9,7 @@ object PlacingParentheses {
     var operators = IndexedSeq[Char]()
     var digits = IndexedSeq[Int]()
 
-    // We created our data structures
+    // We create our data structures
     formula.zipWithIndex.foreach {
       case (c, i) => {
         if (i % 2 == 0) digits = digits :+ c.asDigit
@@ -17,8 +17,6 @@ object PlacingParentheses {
       }
     }
 
-    println(s"operators: ${operators}")
-    println(s"digits: ${digits}")
     val minima = Array.ofDim[Long](digits.length, digits.length)
     val maxima = Array.ofDim[Long](digits.length, digits.length)
     for (i <- 0 until digits.length) {
@@ -31,21 +29,20 @@ object PlacingParentheses {
       var max = Long.MinValue
 
       for (k <- i to j - 1) {
-        println(s"formula for ${digits(i)} and ${digits(j)} with op ${formula(k*2+1)}")
-        val a = evaluateFormula(maxima(i)(k), formula(k * 2 + 1), maxima(k + 1)(j))
-        val b = evaluateFormula(maxima(i)(k), formula(k * 2 + 1), minima(k + 1)(j))
-        val c = evaluateFormula(minima(i)(k), formula(k * 2 + 1), minima(k + 1)(j))
-        val d = evaluateFormula(minima(i)(k), formula(k * 2 + 1), maxima(k + 1)(j))
+        val op = operators(k) // formula(k * 2+1)
+        val a = evaluateFormula(maxima(i)(k), op, maxima(k + 1)(j))
+        val b = evaluateFormula(maxima(i)(k), op, minima(k + 1)(j))
+        val c = evaluateFormula(minima(i)(k), op, minima(k + 1)(j))
+        val d = evaluateFormula(minima(i)(k), op, maxima(k + 1)(j))
         val seq = Seq(a, b, c, d)
         min = Math.min(seq.min, min)
-        max = Math.min(seq.max, max)
+        max = Math.max(seq.max, max)
       }
 
-      println(s"min: ${min} max: ${max}")
       (min, max)
     }
 
-    for (s <- 0 until digits.length) {
+    for (s <- 1 until digits.length) {
       for (i <- 0 until digits.length - s) {
         val j = i + s
         val result = minAndMax(i, j)
