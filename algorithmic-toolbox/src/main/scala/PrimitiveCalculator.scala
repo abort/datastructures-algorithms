7157db2fe626efa1b6291432e3551180a60a428c
Jorryt Dijkstra <abort@Jorryts-Mac-Pro.local>
2017-03-03 15:53:53 +0100

    Completely optimized algorithm

--- algorithmic-toolbox/src/main/scala/PrimitiveCalculator.scala
+++ algorithmic-toolbox/src/main/scala/PrimitiveCalculator.scala
@@ -1,42 +1,55 @@
-import scala.collection.mutable.{Map, Stack}
+import scala.collection.mutable.Stack
 import scala.io.StdIn
 
 object PrimitiveCalculator {
   def main(args: Array[String]): Unit = {
     val result = computeSteps(StdIn.readInt)
-    println(result._1)
-    println(result._2.mkString(" "))
+
+    // Optimal :)
+    println(result._1 + result._2.mkString("\n", " ", ""))
   }
 
   def computeSteps(n : Int) : (Int, Iterable[Int]) = {
-    val stepMap = Map(1 -> 0)
+    // We do not need a map, because we know we will pass all integers (thanks to i + 1), hash code computations slow us down for nothing
+    // We do not use stepMap(0)
+    // We assume we will never reach max value, options would have been nicer (or doubles with infinite), but this has better performance
+    val stepMap = Array.fill[Int](n + 1)(Int.MaxValue)
+    stepMap(1) = 0
 
-    def computeOptimalSteps(x : Int) = Array(x - 1, x / 3 + 1, x / 2 + 1).map(stepMap.get(_).get).min // gets minimum amount of steps for either one of the options
+    def computeOptimalStepCount(x : Int) = {
+      // possible memoized options to use (comprehension might slow us down, but whatever)
+      val seq = (x - 1) +: (for (i <- 2 to 3 if x % i == 0) yield (x / i))
+      // gets minimum amount of steps for either one of the options
+      seq.map(stepMap(_)).min
+    }
+    def precomputeStep(i : Int, value : Int) = if (i <= n) stepMap(i) = Math.min(stepMap(i), value)
 
-    // We compute up to n - 1 (n + 1 is the last step we need to compute for n)
-    for (i <- 1 until n) {
-      val currentStepCount = stepMap.getOrElse(i, computeOptimalSteps(i))
-      val newStepCount = currentStepCount + 1
+    // We compute up to n
+    for (i <- 1 to n) {
+      val currentOptimum = stepMap(i)
+      val optimum = if (currentOptimum != Int.MaxValue) currentOptimum else (computeOptimalStepCount(i) + 1)
+      stepMap(i) = optimum
 
-      stepMap(i * 3) = Math.min(stepMap.getOrElse(i * 3, Int.MaxValue), newStepCount)
-      stepMap(i * 2) = Math.min(stepMap.getOrElse(i * 2, Int.MaxValue), newStepCount)
-      stepMap(i + 1) = Math.min(stepMap.getOrElse(i + 1, Int.MaxValue), newStepCount)
+      // Precompute (not necessary for correct answers, but just to save time)
+      precomputeStep(i * 3, optimum + 1)
+      precomputeStep(i * 2, optimum + 1)
+      precomputeStep(i + 1, optimum + 1)
     }
 
-    var m = n
-    val trace = Stack(m)
-    while (m > 1) {
+    // Build trace
+    val trace = Stack(n)
+    while (trace.head > 1) {
+      val m = trace.head
       val steps = stepMap(m)
-      val possiblePreviousSteps = stepMap.filterKeys(Set(m / 3, m / 2, m - 1).contains(_))
-      // println(s"Possible previous steps for $m: ${possiblePreviousSteps}")
 
-      // Only take candidates with 1 step less and take the smallest value of those (doesn't matter which one)
-      m = possiblePreviousSteps.filter {
+      // Only take candidates with 1 step less and take the smallest value of those (doesn't matter which one), we get tuples of value -> amount of steps
+      val step = Seq(m / 3, m / 2, m - 1).map(v => (v, stepMap(v))).filter {
         case (k, v) => v == steps - 1
-      }.minBy(_._1)._1
-      trace.push(m)
+      }.head._1
+      trace.push(step)
+      // We do not need to take the minimum here due to the invariant of always having an optimum
     }
 
-    (stepMap(n), trace.toArray)
+    (stepMap(n), trace)
   }
 }
