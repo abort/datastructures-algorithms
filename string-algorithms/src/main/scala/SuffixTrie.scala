b957a79979aff826b890ef4af3c62508acea092d
Jorryt Dijkstra <abort@Jorryts-Mac-Pro.local>
2017-09-29 15:24:51 +0200

    optimized

--- string-algorithms/src/main/scala/SuffixTrie.scala
+++ string-algorithms/src/main/scala/SuffixTrie.scala
@@ -4,17 +4,18 @@ object SuffixTrie {
   def main(args: Array[String]): Unit = computeSuffixTrie(StdIn.readLine).foreach(println)
 
   object Node {
-    def Root(sequence : Array[Char]) : Node = new Node(0, 0, sequence.length, Map.empty, None, sequence)
+    def Root(sequence : Array[Char]) : Node = new Node(0, 0, sequence.length, Map.empty, null, sequence)
   }
-  class Node(var start : Int, var end : Int, val level : Int, var children: Map[Char, Node], var parent: Option[Node], val sequence : Array[Char]) {
+  class Node(var start : Int, var end : Int, val level : Int, var children: Map[Char, Node], var parent: Node, val sequence : Array[Char]) {
     def isRoot: Boolean = start == 0 && end == sequence.length
     def value : String = sequence.subSequence(start, end).toString
     def toSequence: Seq[String] = children.values.flatMap(desc => desc.value +: desc.toSequence)(collection.breakOut)
+    def isOnlyChild: Boolean = parent != null && parent.children.size == 1
   }
 
-  def computeSuffixTrie(input : String) : Seq[String] = {
+  def computeSuffixTrie(input : String) : Iterable[String] = {
     val sequence = input.toCharArray
-    var leafs = Set.empty[Node]
+    var leaves = Set.empty[Node]
     var nodes = Set.empty[Node]
     val root = Node.Root(sequence)
 
@@ -25,38 +26,45 @@ object SuffixTrie {
         if (p.children.contains(c)) p = p.children(c)
         else {
           // Spawn
-          val newNode = new Node(i, i + 1, p.level + 1, Map.empty, Some(p), sequence)
+          val newNode = new Node(i, i + 1, p.level + 1, Map.empty, p, sequence)
           p.children = p.children.updated(c, newNode)
-          leafs = leafs - p + newNode
+          leaves = leaves - p + newNode
           p = p.children(c)
           nodes = nodes + newNode
         }
       }
     }
-    compress(leafs, nodes.diff(leafs))
-    root.toSequence
+    val complement = compress(nodes, leaves)
+    // complement.map is faster than toSequence most likely
+    complement.map(_.value)
   }
 
-  private def compress(leafs : Set[Node], nonLeafs : Set[Node]) : Unit = {
-    var toProcess = nonLeafs
+  private def compress(nodes : Set[Node], leaves : Set[Node]) : Set[Node] = {
+    val nonLeaves = nodes.diff(leaves)
+    var toProcess = nonLeaves
+    var complement = nodes
     def compress(node : Node) : Unit = {
       var n = node
-      while (n.parent.isDefined && !n.parent.get.isRoot && n.parent.get.children.size == 1) {
-        //println("Compressing node: " + n.parent.get.characters + s" with adding " + n.characters)
-        n.parent.get.children = n.children
-        n.parent.get.end = n.end
-        //n.parent.get.computeValue()
-        val oldChild = n
-        n = n.parent.get
-        oldChild.parent = None // prevent loitering
-        oldChild.children = Map.empty
+      while (n.parent != null && !n.parent.isRoot && n.isOnlyChild) {
+        val p = n.parent
+        p.children = n.children
+        p.end = n.end
+
+        // n gets deleted
+        val c = n
         toProcess = toProcess - n
+        complement = complement - n
+        n = p
+        c.parent = null // prevent loitering
+        c.children = Map.empty
       }
     }
-
-    leafs.foreach(compress) // filter(_.children.isEmpty) not necessary due to invariant
+    leaves.foreach(compress)
 
     // Sorting to go bottom up in order to be deterministic and compliant to the tests
     toProcess.filter(_.children.size == 1).toSeq.sortBy(-_.level).map(_.children.head._2).foreach(compress)
+
+    // Left over nodes
+    complement
   }
 }
