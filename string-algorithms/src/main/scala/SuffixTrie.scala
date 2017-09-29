908092c43be62409a0210306a3fee0bdd062a165
Jorryt Dijkstra <abort@Jorryts-Mac-Pro.local>
2017-09-29 14:59:11 +0200

    optimized + added tests

--- string-algorithms/src/main/scala/SuffixTrie.scala
+++ string-algorithms/src/main/scala/SuffixTrie.scala
@@ -1,70 +1,62 @@
 import scala.io.StdIn
 
 object SuffixTrie {
-  def main(args: Array[String]): Unit = {
-    computeSuffixTrie(StdIn.readLine).foreach(println)
+  def main(args: Array[String]): Unit = computeSuffixTrie(StdIn.readLine).foreach(println)
+
+  object Node {
+    def Root(sequence : Array[Char]) : Node = new Node(0, 0, sequence.length, Map.empty, None, sequence)
+  }
+  class Node(var start : Int, var end : Int, val level : Int, var children: Map[Char, Node], var parent: Option[Node], val sequence : Array[Char]) {
+    def isRoot: Boolean = start == 0 && end == sequence.length
+    def value : String = sequence.subSequence(start, end).toString
+    def toSequence: Seq[String] = children.values.flatMap(desc => desc.value +: desc.toSequence)(collection.breakOut)
   }
 
-  sealed trait Node {
-    var parent : Option[Node]
-    var children : Map[String, CharNode]
-    val id : Int
-    var characters : String
-
-    def toSequence: Seq[String] = {
-      if (children.isEmpty) Seq.empty
-      else {
-        // s"$id->${v.id}:${v.characters}"
-        //children.map { case (k,v) => s"$id->${v.id}:${v.characters}" }.toSeq ++ children.values.flatMap(_.toSequence).toSeq
-        children.values.flatMap(desc => desc.characters +: desc.toSequence)(collection.breakOut)
-      }
-    }
-  }
-  class CharNode(override var characters: String, override var children: Map[String, CharNode], override val id : Int, override var parent: Option[Node]) extends Node
-  class Root(override var children: Map[String, CharNode]) extends Node {
-    override val id : Int = 0
-    override var parent : Option[Node] = None
-    override var characters : String = ""
-  }
-
-  def computeSuffixTrie(input : String) : Seq[String] = computeSuffixTrie(input.indices.map(i => input.substring(i)))
-
-  private def computeSuffixTrie(sequences : Seq[String]) = {
-    println("inputs: " + sequences.mkString("[", ", ", "]"))
+  def computeSuffixTrie(input : String) : Seq[String] = {
+    val sequence = input.toCharArray
     var leafs = Set.empty[Node]
-    val root = new Root(Map.empty)
-    var count = 1
+    var nodes = Set.empty[Node]
+    val root = Node.Root(sequence)
 
-    for (charSequence <- sequences.map(_.toCharArray)) {
+    for (offset <- sequence.indices) {
       var p : Node = root
-      for (c <- charSequence) {
-        val asString = c.toString
-        if (p.children.contains(asString)) p = p.children(asString)
+      for (i <- offset until sequence.length) {
+        val c = sequence.charAt(i)
+        if (p.children.contains(c)) p = p.children(c)
         else {
           // Spawn
-          val newNode = new CharNode(asString, Map.empty, count, Some(p))
-          p.children = p.children.updated(asString, newNode)
+          val newNode = new Node(i, i + 1, p.level + 1, Map.empty, Some(p), sequence)
+          p.children = p.children.updated(c, newNode)
           leafs = leafs - p + newNode
-          count += 1
-          p = p.children(asString)
+          p = p.children(c)
+          nodes = nodes + newNode
         }
       }
     }
-    leafs.filter(_.children.isEmpty).foreach(compress)
+    compress(leafs, nodes.diff(leafs))
     root.toSequence
   }
 
-  def compress(node : Node) : Node = {
-    if (node.children.isEmpty) {
+  private def compress(leafs : Set[Node], nonLeafs : Set[Node]) : Unit = {
+    var toProcess = nonLeafs
+    def compress(node : Node) : Unit = {
       var n = node
-      while (n.parent.isDefined && n.parent.get.children.size == 1) {
+      while (n.parent.isDefined && !n.parent.get.isRoot && n.parent.get.children.size == 1) {
         //println("Compressing node: " + n.parent.get.characters + s" with adding " + n.characters)
-        n.parent.get.children = Map.empty
-        println(s"\tSet ${n.parent.get.characters} to ${n.parent.get.characters + n.characters}")
-        n.parent.get.characters = n.parent.get.characters + n.characters
+        n.parent.get.children = n.children
+        n.parent.get.end = n.end
+        //n.parent.get.computeValue()
+        val oldChild = n
         n = n.parent.get
+        oldChild.parent = None // prevent loitering
+        oldChild.children = Map.empty
+        toProcess = toProcess - n
       }
     }
-    node
+
+    leafs.foreach(compress) // filter(_.children.isEmpty) not necessary due to invariant
+
+    // Sorting to go bottom up in order to be deterministic and compliant to the tests
+    toProcess.filter(_.children.size == 1).toSeq.sortBy(-_.level).map(_.children.head._2).foreach(compress)
   }
 }
