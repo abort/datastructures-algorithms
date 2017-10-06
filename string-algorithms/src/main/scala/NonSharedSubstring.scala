78e6c339576255b875b5877fd639be11c91ef514
Jorryt Dijkstra <abort@Jorryts-Mac-Pro.local>
2017-10-06 20:23:57 +0200

    Non-shared substring search finished

--- string-algorithms/src/main/scala/NonSharedSubstring.scala
+++ string-algorithms/src/main/scala/NonSharedSubstring.scala
@@ -6,52 +6,45 @@ object NonSharedSubstring {
   def main(args: Array[String]): Unit = {
     val sequence = StdIn.readLine
     val comparisonSequence = StdIn.readLine
+
     val output = computeShortestNonSharedSubString(sequence, comparisonSequence)
     if (output.isEmpty) println("Provided sequences are the same")
     else println(output.get)
-/*
-    var output : Option[String] = None
-    import org.scalameter._
-    val time = measure {
-      output = computeShortestNonSharedSubString(sequence, comparisonSequence)
-    }
-    if (output.isEmpty) println("Provided sequences are the same")
-    else println(output.get)
-    println(s"Took $time to compute")
-  */
   }
+
 
   object Node {
     def Root(sequence : Array[Char]) : Node = new Node(0, 0, Map.empty, null, sequence)
   }
   class Node(var start : Int, var end : Int, var children: Map[Char, Node], var parent: Node, val sequence : Array[Char]) {
+    lazy val isRoot: Boolean = start == 0 && end == 0
     lazy val length = end - start
-    def isRoot: Boolean = start == 0 && end == 0
     lazy val value : String = sequence.subSequence(start, end).toString
+    def asSequence: List[String] = children.values.flatMap(desc => desc.value +: desc.asSequence)(collection.breakOut)
+
     @tailrec
-    final def contains(input : String, accumulated : String = "") : Boolean = {
-//      println(s"in $value node")
-      if (isRoot) {
-//        println(s"checking for ${input.head}")
-        if (children.contains(input.head)) children(input.head).contains(input, value)
-        else false
-      }
-      else if (length >= input.length && value.substring(0, input.length) == input) true
-      else if (input.take(length) == value) {
-//        println(s"value: $value")
-        // dispatch rest to child
-        val remainder = input.drop(length)
-//        println(s"Remainder: $remainder")
-        if (remainder.isEmpty) true
-        else if (children.contains(remainder.head)) children(remainder.head).contains(remainder, value)
-        else false
+    final def traverse(s : String, accumulated : String = "") : Option[String] = {
+      if (s.isEmpty) None
+      else if (isRoot) {
+        if (children.contains(s.head)) children(s.head).traverse(s, accumulated)
+        else Some(s.head.toString)
       }
       else {
-//        println("Else")
-        false
+        val consumed = value.zipWithIndex.takeWhile { case (c, i) => i < s.length && c == s(i) }.map(_._1).mkString
+        if (s.length == consumed.length) None
+        else if (s.length > value.length) {
+          if (consumed.length == length && children.contains(s(consumed.length))) {
+            children(s(consumed.length)).traverse(s.substring(consumed.length), accumulated + value)
+          }
+          else {
+            Some(accumulated + consumed + s(consumed.length))
+          }
+        }
+        else if (s.length < length) Some(accumulated + consumed + s(consumed.length))
+        else if (consumed.length < length) Some(accumulated + consumed + s(consumed.length))
+        else None
       }
     }
-    def asSequence: List[String] = children.values.flatMap(desc => desc.value +: desc.asSequence)(collection.breakOut)
   }
 
   def getCommonStringSkip(startPos : Int, existingStartPos : Int, existingEndPos : Int, sequence : Array[Char]) : Int = {
@@ -67,8 +60,7 @@ object NonSharedSubstring {
     i
   }
 
-  def insertSequence(root : Node, offset : Int, sequence : Array[Char]) : List[Node] = {
-    var addedNodes = List.empty[Node]
+  def insertSequence(root : Node, offset : Int, sequence : Array[Char]) : Unit = {
     var node = root
     var index = offset
 
@@ -88,7 +80,6 @@ object NonSharedSubstring {
         // Split on common string and spawn new parent for it
         val key = sequence(subNode.start + commonLen)
         val newParent = new Node(index, splitPoint, Map(key -> subNode), null, sequence)
-        addedNodes = addedNodes :+ newParent
         subNode.start = subNode.start + commonLen
         node.children = node.children.updated(c, newParent)
         index = splitPoint
@@ -98,47 +89,30 @@ object NonSharedSubstring {
     }
     // Spawn new child as it does not exist yet
     val newNode = new Node(index, sequence.length, Map.empty, null, sequence)
-    addedNodes = addedNodes :+ newNode
     node.children = node.children.updated(sequence(index), newNode)
-
-    addedNodes
   }
 
-  def computeShortestNonSharedSubString(sequence : String, comparisonSequence : String) : Option[String] = {
-    val root = computeSuffixTrie(comparisonSequence)
-    allSubstrings(sequence).find(!root.contains(_))
-  }
+  def computeShortestNonSharedSubString(input : String, comparisonSequence : String) : Option[String] = {
+    val sequence = if (input.startsWith(comparisonSequence)) input.substring(comparisonSequence.length) else input
 
-  def allSubstrings(input : String) : Seq[String] = {
+    val root = computeSuffixTrie(comparisonSequence + '$')
     val ordering : Ordering[String] = Ordering.by { s => s.length() }
-    val queue = mutable.PriorityQueue.empty[String](ordering.reverse)
-    var processed = Set.empty[String]
-    for (i <- input.indices) {
-      for (j <- i until input.length) {
-        val seq = input.substring(i, j + 1)
-        if (!processed.contains(seq)) {
-          processed = processed + seq
-          queue.enqueue(seq)
-        }
+    val results  = mutable.PriorityQueue.empty[String](ordering.reverse)
+    for (i <- sequence.indices) {
+      val result = root.traverse(sequence.substring(i))
+      if (result.isDefined) {
+        results.enqueue(result.get)
+        if (result.get.length == 1) return result // we won't find a smaller example than this for sure :)
       }
     }
-    var output = Seq.empty[String]
-    while (queue.nonEmpty) {
-      output = output :+ queue.dequeue()
-    }
-    output
+    if (results.nonEmpty) Some(results.dequeue()) else None
   }
 
   def computeSuffixTrie(input : String) : Node = {
-    val augmented = input + '$'
-    val sequence = augmented.toCharArray
+    val sequence = input.toCharArray
     val root = Node.Root(sequence)
 
-    var nodes = List.empty[Node]
-    for (offset <- sequence.indices) {
-      val addedNodes = insertSequence(root, offset, sequence)
-      nodes = addedNodes ++ nodes
-    }
+    sequence.indices.foreach(insertSequence(root, _, sequence))
 
     root
   }
