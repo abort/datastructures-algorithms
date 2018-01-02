30625a703f6272640317b8cbb6b5b9fae65317df
abortji <abort@digitalise.net>
2018-01-02 17:31:31 +0100

    some optimizations on inverse bwt

--- string-algorithms/src/main/scala/InverseBWT.scala
+++ string-algorithms/src/main/scala/InverseBWT.scala
@@ -5,36 +5,18 @@ object InverseBWT {
   def main(args: Array[String]): Unit = println(computeInverseBurrowsWheelerTransform(StdIn.readLine))
 
   def computeInverseBurrowsWheelerTransform(input : String) : String = {
-    val firstColumns = sort(input.toCharArray, input.min, input.max)
-    val firstColumnsZipped = firstColumns.zipWithIndex
-    val lastColumns = input.toIndexedSeq
+    val firstColumnsZipped = input.zipWithIndex
+    val first = firstColumnsZipped.sortBy(_._1)
+    val firstToLast = first.zip(firstColumnsZipped).toMap
 
-    val indexedCharToChar = collection.mutable.Map[(Char, Int), (Char, Int)](firstColumnsZipped.map(_ -> null) : _*)
-    val elements = firstColumns.map((_, 0))
-    val indexedFirstChars = collection.mutable.Map(elements : _*)
-    val indexedLastChars = collection.mutable.Map(elements : _*)
-
-    firstColumnsZipped.foreach { case (c, i) =>
-      val occurrence = indexedFirstChars(c)
-      val subject = lastColumns(i)
-      var subjectOccurrence = indexedLastChars(subject)
-      indexedCharToChar((c, occurrence)) = (subject, subjectOccurrence)
-      indexedFirstChars(c) = occurrence + 1
-      indexedLastChars(subject) = subjectOccurrence + 1
+    var pointer = first.head
+    val result = Array.ofDim[Char](input.length)
+    for (i <- 0 until input.length) {
+      result(input.length - i - 1) = pointer._1
+      pointer = firstToLast(pointer)
     }
 
-    val delimitingKey = (delimiter, 0)
-    var key = delimitingKey
-    val result = new StringBuilder(input.length)
-    do {
-      val entry = indexedCharToChar(key)
-      val next = entry
-
-      result.insert(0, key._1)
-      key = next
-    } while (key != delimitingKey)
-
-    result.toString
+    String.valueOf(result)
   }
 
   private def sort(a: Array[Char], min: Char, max: Char): Array[Char] = {
